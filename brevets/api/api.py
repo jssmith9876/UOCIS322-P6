e1fa58e0b7a30b102d9120bbfdb3fe12916e6d33
Jordan Smith <jssmith9876@yahoo.com>
2021-05-28 20:57:12 -0700

    All APIs should be implemented

--- brevets/api/api.py
+++ brevets/api/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 from flask_restful import Resource, Api
 import logging
 from pymongo import MongoClient
@@ -17,62 +17,52 @@ def get_times():
 ###
 # APIs
 ###
-# class test(Resource):
-#     def get(self):
-#         return "Nice"
-
-# api.add_resource(test, '/test')
-
-@api.representation('application/json')
-def return_json(data):
-    return jsonify(data)
-
-@api.representation('application/csv')
-def return_csv(data, headers):
-    return data
-
 def get_API_results(desired_keys, return_type):
     # Get the times from the db
     data = get_times()
 
-    # Fill the result with only opening and closing times
+    # Fill the result with only desired values
+    desired_keys += ['miles', 'km']
     times = {}
     for entry in data:
         ind = int(entry['index'])
         times[ind] = {key: entry[key] for key in desired_keys}
 
-    # Return the json
     app.logger.debug(f"Got a {return_type} request")
-    if (return_type == 'json'):
-        return return_json(times)
-    # Return csv
-    elif (return_type == 'csv'):
-        return return_csv(times, desired_keys)
 
-# Returns a json list of all open and close times
+    # Return the json
+    if (return_type == 'json'):
+        return jsonify(times)
+    
+    # Return the csv
+    elif (return_type == 'csv'):
+        csv_str = ",".join(desired_keys) + "\n"
+        for time in times.values():
+            for key in desired_keys:
+                csv_str += time[key] + ","
+            csv_str += "\n"
+        return Response(csv_str, mimetype='text/plain')
+
+# Returns a list of all open and close times
 class listAll(Resource):
     def get(self, file_type='json'):
         return get_API_results(['open', 'close'], file_type)
 
-# Returns a json list of all open times
+# Returns a list of all open times
 class listOpenOnly(Resource):
     def get(self, file_type='json'):
         return get_API_results(['open'], file_type)
 
-# Returns a json list of all close times
+# Returns a list of all close times
 class listCloseOnly(Resource):
     def get(self, file_type='json'):
         return get_API_results(['close'], file_type)
 
-# Default list all
-api.add_resource(listAll, '/listAll')
-api.add_resource(listOpenOnly, '/listOpenOnly')
-api.add_resource(listCloseOnly, '/listCloseOnly')
+# List of resources
+api.add_resource(listAll, '/listAll', '/listAll/<string:file_type>')
+api.add_resource(listOpenOnly, '/listOpenOnly', '/listOpenOnly/<string:file_type>')
+api.add_resource(listCloseOnly, '/listCloseOnly', '/listCloseOnly/<string:file_type>')
 
-# Specified list all
-# api.add_resource(listAll, '/listAll/<string:file_type>')
-# api.add_resource(listOpenOnly, '/listOpenOnly/<string:file_type>')
-# api.add_resource(listCloseOnly, '/listCloseOnly/<string:file_type>')
 
 if app.debug:
     app.logger.setLevel(logging.DEBUG)
